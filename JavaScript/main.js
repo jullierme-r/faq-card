e787adc8e6b5b8cd1f50c48216111c44340e841d
Jullierme Rodrigues Gomes <jullierme.r.gomes@gmail.com>
2021-08-26 17:58:55 -0400

    Update main.js

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -5,13 +5,11 @@ let button = document.querySelectorAll(".faq-btn");
 function showFaqAnswers() {
     let open = this.classList.toggle("isOpen"); //toggle the class isOpen and storages the state inside open
     let answerDiv = this.nextElementSibling; //storages the content of the button's sibling
-    
+
     if(open){ //if the button has the class 'isOpen', adds to the sibling the class to make the answer visible
         answerDiv.classList.add("show-faq-answer");
-        this.style.fontWeight = 700;
     } else { // otherwise remove the class, hiding the answer
         answerDiv.classList.remove("show-faq-answer");
-        this.style.fontWeight = 400;
     }
 }
 
