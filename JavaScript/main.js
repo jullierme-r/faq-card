e1d378259b07011579a91e25ec90e2c692c74396
Jullierme Rodrigues Gomes <jullierme.r.gomes@gmail.com>
2021-09-08 13:26:06 -0400

    Update main.js

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,6 +1,7 @@
 "use strict";
 
 let button = document.querySelectorAll(".faq-btn");
+let box = document.querySelector("#box");
 
 function showFaqAnswers() {
     let open = this.classList.toggle("isOpen"); //toggle the class isOpen and storages the state inside open
@@ -8,9 +9,17 @@ function showFaqAnswers() {
 
     if(open){ //if the button has the class 'isOpen', adds to the sibling the class to make the answer visible
         answerDiv.classList.add("show-faq-answer");
+        this.setAttribute("aria-expanded", "true");
     } else { // otherwise remove the class, hiding the answer
         answerDiv.classList.remove("show-faq-answer");
+        this.setAttribute("aria-expanded", "false");
     }
 }
 
+function boxEffect(){
+    box.classList.toggle("box-hover");
+}
+
 button.forEach((element) => element.addEventListener("click", showFaqAnswers));
+button.forEach((element) => element.addEventListener("mouseover", boxEffect));
+button.forEach((element) => element.addEventListener("mouseout", boxEffect));
