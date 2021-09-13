73d28ea2eef2c3ad4fde0e7fbc8b0bd8ab080748
Jullierme Rodrigues Gomes <jullierme.r.gomes@gmail.com>
2021-09-13 17:00:05 -0400

    Update main.js

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -3,23 +3,29 @@
 let button = document.querySelectorAll(".faq-btn");
 let box = document.querySelector("#box");
 
-function showFaqAnswers() {
-    let open = this.classList.toggle("isOpen"); //toggle the class isOpen and storages the state inside open
-    let answerDiv = this.nextElementSibling; //storages the content of the button's sibling
-
-    if(open){ //if the button has the class 'isOpen', adds to the sibling the class to make the answer visible
-        answerDiv.classList.add("show-faq-answer");
-        this.setAttribute("aria-expanded", "true");
-    } else { // otherwise remove the class, hiding the answer
-        answerDiv.classList.remove("show-faq-answer");
-        this.setAttribute("aria-expanded", "false");
-    }
+function openFaq() {
+    //to ensure that only one item will remains open at the time, set all the items to the closed state
+    button.forEach(e => {
+        if(e.classList.contains("isOpen")){
+            e.classList.remove("isOpen");
+            e.setAttribute("aria-expanded", "false");
+            e.nextElementSibling.classList.remove("show-faq-answer");
+        }
+    });
+    
+    this.classList.toggle("isOpen"); //the button the was suffered the action receive the class 'isOpen' to indicate that it will be displayed
+    let answerDiv = this.nextElementSibling; //storages the content of the button's next sibling
+    answerDiv.classList.toggle("show-faq-answer"); //gives to the button's sibling the class to display the content
+    this.setAttribute("aria-expanded", "true"); //updates the aria-expanded of the clicked button
 }
 
 function boxEffect(){
     box.classList.toggle("box-hover");
 }
 
-button.forEach((element) => element.addEventListener("click", showFaqAnswers));
-button.forEach((element) => element.addEventListener("mouseover", boxEffect));
-button.forEach((element) => element.addEventListener("mouseout", boxEffect));
+button.forEach((e) => e.addEventListener("click", openFaq));
+button.forEach((e) => e.addEventListener("mouseover", boxEffect));
+button.forEach((e) => e.addEventListener("mouseout", boxEffect));
+
+
+
