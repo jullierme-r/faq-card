9d9274d990ba550382a64ccade128b5fc4cfe959
Jullierme Rodrigues Gomes <jullierme.r.gomes@gmail.com>
2021-09-16 18:30:30 -0400

    Update main.js

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -3,7 +3,7 @@
 let button = document.querySelectorAll(".faq-btn");
 let box = document.querySelector("#box");
 
-function openFaq() {
+function closeOpenedFaq () {
     //to ensure that only one item will remains open at the time, set all the items to the closed state
     button.forEach(e => {
         if(e.classList.contains("isOpen")){
@@ -12,11 +12,23 @@ function openFaq() {
             e.nextElementSibling.classList.remove("show-faq-answer");
         }
     });
-    
-    this.classList.toggle("isOpen"); //the button the was suffered the action receive the class 'isOpen' to indicate that it will be displayed
+}
+
+function openFaq() {
     let answerDiv = this.nextElementSibling; //storages the content of the button's next sibling
-    answerDiv.classList.toggle("show-faq-answer"); //gives to the button's sibling the class to display the content
-    this.setAttribute("aria-expanded", "true"); //updates the aria-expanded of the clicked button
+
+    if(this.classList.contains("isOpen")){ //if the button is already open, removes all the classes that makes the content visible
+        this.classList.toggle("isOpen");
+        this.setAttribute("aria-expanded", "false");
+        answerDiv.classList.toggle("show-faq-answer");
+    } else {
+        closeOpenedFaq(); 
+
+        //updates aria-expanded and classes of the clicked button and its sibling to be displayed
+        this.classList.toggle("isOpen");
+        this.setAttribute("aria-expanded", "true");
+        answerDiv.classList.toggle("show-faq-answer");
+    }
 }
 
 function boxEffect(){
